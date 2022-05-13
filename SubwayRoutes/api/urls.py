242e6002bdc00dbb5dfe12ed879df51fe44701c3
Niru1095 <niranjanghag.ng@gmail.com>
2022-05-13 12:01:04 +0530

    Changes

--- SubwayRoutes/api/urls.py
+++ SubwayRoutes/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home, routes , most_stops, fewest_stops
+from .views import home, routes , most_stops, fewest_stops , two_more_routes
 
 urlpatterns = [
     path('',home ),
     path('routes/',routes ),
     path('most_stops/',most_stops ),
     path('fewest_stops/',fewest_stops ),
+    path('two_more_routes/',two_more_routes ),
 ]
--- SubwayRoutes/api/views.py
+++ SubwayRoutes/api/views.py
@@ -50,6 +50,21 @@ def min_stops(data):
     min_count = min(count_dict, key= lambda x: count_dict[x])
     return min_count
 
+# Function to finds subway routes from single direction_name with two or more stops
+def count_2_more(data):
+    count_dict = {}
+    count = 1
+    for val in data:
+        lst = list(val.values())[1]
+        dict_key = str(lst)
+        if dict_key in count_dict:
+            count_dict[dict_key] = count_dict[dict_key] + 1
+        else:
+            count_dict[dict_key] = count
+    # count2 = min(count_dict, key= lambda x: count_dict[x])
+    filtered_dict = {k:v for (k,v) in count_dict.items() if v>=2 }
+    return list(filtered_dict)
+
 
 # Program list subway routes with the most stops.
 @api_view()
@@ -73,5 +88,15 @@ def fewest_stops(request):
     filter_data = list(filter(lambda data: str(data['direction_names']) == minn, data))
     return Response(data=filter_data)
 
+# Program list subway routes with the two or more stops.
+@api_view()
+def two_more_routes(request):
+    api_link = "https://api-v3.mbta.com/routes?filter[type]=0,1"
+    response_data = requests.get(api_link).json()
+    api_dict = response_data['data']
+    data = [{'long_name':res['attributes']['long_name'],'direction_names': res['attributes']['direction_names'] } for res in api_dict]
+    two_more = count_2_more(data)
+    filter_data = list(filter(lambda data: str(data['direction_names']) in two_more, data))
+    return Response(data=filter_data)
 
     
